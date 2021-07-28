80bf9f7b66a6d2467f89b97668fb4b87e2354c50
Thomas Steinert <hello@chroni.cc>
2021-07-28 13:37:21 +0200

    Add kind cluster to support k8s evaluations

---
+++ kind/start-cluster.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+kind create cluster --config config.yaml
+
---
+++ kind/stop-cluster.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+CLUSTER_NAME=$(grep name: config.yaml | cut -d' ' -f2)
+kind delete cluster --name $CLUSTER_NAME
+
