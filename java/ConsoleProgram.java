334c08af097a51dd005d482a77d2beebcd0231af
Thomas Steinert <hello@chroni.cc>
2021-09-02 13:49:41 +0200

    Add simple java console program for testing input and output

---
+++ java/ConsoleProgram.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class ConsoleProgram {
+    public static void main(String args[]) {
+        Console console = System.console();
+        System.out.println(console);
+    }
+}
+
