b738b386ac0e9d4596a777fd5d598caf0f1b6ab4
Thomas Steinert <hello@chroni.cc>
2021-09-17 08:50:02 +0200

    Add example for python REPL debugging

---
+++ python/repl_debugging/README.md
@@ -0,0 +1,4 @@
+# REPL Debugging
+
+Source: https://jvns.ca/blog/2021/09/16/debugging-in-a-repl-is-fun/
+
---
+++ python/repl_debugging/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import requests
+
+def make_request():
+    result = requests.get("https://google.com")
+    import ipdb; ipdb.set_trace()
+
+make_request()
+
