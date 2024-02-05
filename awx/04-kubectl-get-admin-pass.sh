2c034471341a5f0e1fe9909288782785e0524845
Thomas Steinert <hello@chroni.cc>
2024-02-05 19:53:47 +0100

    fix: use correct name for awx admin pass secret

--- awx/04-kubectl-get-admin-pass.sh
+++ awx/04-kubectl-get-admin-pass.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-kubectl -n awx get secrets awx-demo-admin-password -o jsonpath="{.data.password}" | base64 -d; echo
+kubectl -n awx get secrets awx-admin-password -o jsonpath="{.data.password}" | base64 -d; echo
