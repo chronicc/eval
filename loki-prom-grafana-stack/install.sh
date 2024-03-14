8311605b8a27b7e43eab8332f0097c31734ca2b2
Thomas Steinert <hello@chroni.cc>
2024-03-14 14:39:58 +0100

    fix(loki-prom-grafana-stack): install the certificates after the namespace is created

--- loki-prom-grafana-stack/install.sh
+++ loki-prom-grafana-stack/install.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-echo "\n>>> Adding and updating helm repos\n"
+echo -e "\n>>> Adding and updating helm repos\n"
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
-echo "\n>>> Updating tls certificates\n"
-kubectl -n monitoring delete secrets grafana-tls
-kubectl -n monitoring create secret tls grafana-tls --key ../ssl-ca/grafana.kubernetes.local.key --cert ../ssl-ca/grafana.kubernetes.local.crt
-
-echo "\n>>> Installing helm charts\n"
+echo -e "\n>>> Installing helm charts\n"
 helm upgrade --install -n monitoring --create-namespace my-loki \
   grafana/loki-stack --values loki-values.yml
 helm upgrade --install -n monitoring --create-namespace my-prometheus \
   prometheus-community/kube-prometheus-stack --values prometheus-values.yml
 
-echo "\n>>> Installing grafana dashboards\n"
+echo -e "\n>>> Updating tls certificates\n"
+kubectl -n monitoring delete secrets grafana-tls
+kubectl -n monitoring create secret tls grafana-tls --key ../ssl-ca/grafana.kubernetes.local.key --cert ../ssl-ca/grafana.kubernetes.local.crt
+
+echo -e "\n>>> Installing grafana dashboards\n"
 kubectl -n monitoring apply -f dashboard-loki.yml
